0df6a4b1c1f9e50e79877150372c691eddbb26c9
samuelkala <samuel.kala@mail.polimi.it>
2022-05-06 00:42:37 +0200

    show Languages and Voices modified

--- public/manage_azure.js
+++ public/manage_azure.js
@@ -8,17 +8,60 @@
     let languageOptions = document.getElementById('languageOptions');
     let voiceOptions = document.getElementById('voiceOptions');
     let mapLanguageName = new Map();
+    let allVoices = [];
+    //not to send to the server because it is not needed for SSML
+    let selectedLanguage;
+    let selectedVoice;
+    let selectedSpeed;
+    let selectedPitch;
+    let speakingStyle;
     let settings = [];
 
-    let fillMap = function(info){
-        if(info[0].LocaleName != undefined){
+    let getAllVoices = function (info, allVoices){
+        info.forEach((element, index) =>{
+            allVoices.push(element.ShortName);
+        });
+    }
+
+    let loadVoices = function (language) {
+        let voices = mapLanguageName.get(language);
+        voiceOptions.innerHTML = "";
+        // display voices for clicked language
+        voices.forEach((element, index) => {
+            voiceOptions.innerHTML += "<option value=\"" + element.ShortName + "\">" +
+                element.LocalName + "</option>";
+        });
+
+        selectedVoice = voices[0].ShortName;
+        console.log(selectedVoice);
+    }
+
+    let loadLanguages = function () {
+        languageOptions.innerHTML = "";
+        let languages = Array.from(mapLanguageName.keys());
+        if (languages.includes('English (United States)')) {
+            selectId = languages.indexOf('English (United States)');
+        } else {
+            selectId = 0;
+        }
+        selectedLanguage = languages.at(selectId);
+        mapLanguageName.forEach((value, key) => {
+            languageOptions.innerHTML += "<option value=\"" + key + "\">" + key + "</option>";
+        });
+        languageOptions.selectedIndex = selectId;
+        languageOptions.disabled = false;
+        loadVoices(selectedLanguage);
+    }
+
+    let fillMap = function (info) {
+        if (info[0].LocaleName != undefined) {
             let curLocale = info[0].LocaleName;
             let names = [];
-            info.forEach((element,index) =>{
-                if(curLocale === element.LocaleName){
+            info.forEach((element, index) => {
+                if (curLocale === element.LocaleName) {
                     names.push(new Name(element.LocalName, element.ShortName));
-                }else{
-                    mapLanguageName.set(`${curLocale}`,names);
+                } else {
+                    mapLanguageName.set(`${curLocale}`, names);
                     names = new Array();
                     curLocale = element.LocaleName;
                 }
@@ -27,71 +70,67 @@
     };
 
 
-    function Name(LocalName, ShortName){
+    function Name(LocalName, ShortName) {
         this.LocalName = LocalName;
         this.ShortName = ShortName;
-    }   
-    
-    
-    function Settings(language, voice, speed, pitch){
+    }
+
+
+    function Settings(language, voice, speed, pitch) {
         this.language = language;
         this.voice = voice;
         this.speed = speed;
         this.pitch = pitch;
     }
 
-    (async function getAuthorizationToken(){
-        try{
+    (async function getAuthorizationToken() {
+        try {
             const response = await fetch(authorizationendpoint);
             const info = await response.json();
             authorizationToken = info.token;
             region = info.region;
             console.log('Token fetched from back-end: ' + authorizationToken);
-        }catch(err){
+        } catch (err) {
             console.log(err);
             errorAlert.innerHTML = 'error while getting authorization Token';
         }
     })();
 
     getVoices.addEventListener('click', async () => {
-        try{
+        try {
             const response = await fetch('https://' + region + ".tts.speech." +
-            (region.startsWith("china") ? "azure.cn" : "microsoft.com") +
-                    "/cognitiveservices/voices/list",{
+                (region.startsWith("china") ? "azure.cn" : "microsoft.com") +
+                "/cognitiveservices/voices/list", {
                 method: 'GET',
                 headers: {
-                    'Authorization' : 'Bearer ' + authorizationToken
+                    'Authorization': 'Bearer ' + authorizationToken
                 }
             });
             const info = await response.json();
+            getAllVoices(info, allVoices);
             fillMap(info);
-            mapLanguageName.forEach((value, key) =>{
-                languageOptions.innerHTML += "<option value=\"" + key + "\">" + key + "</option>";
-            });
-            languageOptions.disabled = false;
+            loadLanguages();
             console.log('hello');
-        }catch(err){
+        } catch (err) {
             console.log(err);
             console.log('error during the retrieving the voices');
         }
     })
-    
+
     //this method triggers the language option in order to display
     //the correct voices for a particular language
     document.addEventListener('click', function (event) {
         if (Array.from(mapLanguageName.keys()).includes(event.target.value)) {
-            let voices = mapLanguageName.get(`${event.target.value}`);
-            voiceOptions.innerHTML = "";
-            // display voices for clicked language
-            voices.forEach((element, index) => {
-                voiceOptions.innerHTML += "<option value=\"" + element.LocalName + "\">" + 
-                element.LocalName + "</option>";
-            });
+            loadVoices(`${event.target.value}`);
+        }
+        if(allVoices.includes(event.target.value)){
+            selectedVoice = event.target.value;
+            console.log(selectedVoice);
         }
     }, false);
 
 
-    
+
 
 
 
