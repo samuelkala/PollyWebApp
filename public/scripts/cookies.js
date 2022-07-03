f4010f8f53b1da6a4fbf64b3e67f93d870796b28
samuelkala <samuel.kala@mail.polimi.it>
2022-07-03 19:04:37 +0200

    little change on cookie file

--- public/scripts/cookies.js
+++ public/scripts/cookies.js
@@ -1,26 +1,18 @@
 function setCookie(name, value) {
-    // Encode value in order to escape semicolons, commas, and whitespace
     let cookie = name + "=" + encodeURIComponent(value);
     cookie += "; max-age=" + (365 * 24 * 60 * 60);
     document.cookie = cookie;
 }
 
 function getCookie(name) {
-    // Split cookie string and get all individual name=value pairs in an array
     let cookieArr = document.cookie.split(";");
     let savedSettings;
-    // Loop through the array elements
     for (let i = 0; i < cookieArr.length; i++) {
         let cookiePair = cookieArr[i].split("=");
-        /* Removing whitespace at the beginning of the cookie name
-        and compare it with the given string */
         if (name == cookiePair[0].trim()) {
-            // Decode the cookie value and return
             savedSettings = decodeURIComponent(cookiePair[1]);
             return JSON.parse(savedSettings);
         }
     }
-
-    // Return null if not found
     return null;
 }
